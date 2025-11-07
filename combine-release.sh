40e86db8f1f46a7b2ff607607a806601df1bd874
Kento Chiba <c.kento.chiba@gmail.com>
2025-11-08 01:10:17 +0900

    delete everything

--- combine-release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Combining ZIP file parts..."
-cd release
-cat LoL-Analytics-Viewer-Windows-x64.zip.part-* > LoL-Analytics-Viewer-Windows-x64.zip
-echo "Done! File created: release/LoL-Analytics-Viewer-Windows-x64.zip"
